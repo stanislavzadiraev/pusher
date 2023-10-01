bde68bcd689f7cf24296d89b983a680d08d9a9c9
stanislavzadiraev <zadiraeff@yandex.ru>
2023-10-01 17:42:55 +0300

    добавлено

---
+++ dst/index.js
@@ -0,0 +1,12 @@
+import {mkdir, rmdir} from 'node:fs/promises'
+
+const MKDIR = $ => mkdir($, {recursive: true}).catch(N)
+const RMDIR = $ => rmdir($, {recursive: true}).catch(N)
+
+const build = ({}) => 
+    MKDIR('bin')
+
+const prune = ({}) => 
+    MKDIR('bin')
+
+export default {build, prune}
