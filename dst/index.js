e14a08e7b3ebce7140eb9282caa99167bdeed8eb
stanislavzadiraev <zadiraeff@yandex.ru>
2023-10-02 20:11:02 +0300

    добавлено

--- dst/index.js
+++ dst/index.js
@@ -1,14 +1,21 @@
-import {mkdir, rmdir} from 'node:fs/promises'
+import {mkdir, rmdir, readFile, writeFile} from 'node:fs/promises'
 
 const N = () => undefined
 
 const MKDIR = $ => mkdir($, {recursive: true}).catch(N)
 const RMDIR = $ => rmdir($, {recursive: true}).catch(N)
 
+
 const build = ({}) => 
     MKDIR('bin')
+    .then(() =>
+        readFile('./node_modules/mdlcfg/bin/index.js')
+    )
+    .then(data =>
+        writeFile('./bin/index.js')
+    )
 
 const prune = ({}) => 
-    MKDIR('bin')
+    RMDIR('bin')
 
 export default {build, prune}
