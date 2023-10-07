4610db1cd2d5892cf76e2fef61aa0ac432703f7f
stanislavzadiraev <zadiraeff@yandex.ru>
2023-10-07 21:48:58 +0300

    обновлено и добавлено

--- bin/index.js
+++ bin/index.js
@@ -1,35 +1,39 @@
 #!/usr/bin/env node
 
-process.title = 'node'
-process.removeAllListeners('warning')
+process.removeAllListeners("warning");
 
-const I = ' - '
-const C = '../'
+const N = () => undefined;
 
-const curpath = new Array(1).fill(C).join('')
-const parpath = new Array(3).fill(C).join('')
+const I = " - ";
+const C = "../";
 
-const mdlname = ['src', 'dst'][1]
+const curpath = new Array(1).fill(C).join("");
+const parpath = new Array(3).fill(C).join("");
 
-Promise
-.all([
-  import(curpath + 'package.json', { assert: { type: 'json' } }),
-  import(parpath + 'package.json', { assert: { type: 'json' } }),
+const theslug =
+	//  "src";
+	"dst";
+
+Promise.all([
+	import(curpath + "package.json", { assert: { type: "json" } }),
+	import(parpath + "package.json", { assert: { type: "json" } }),
 ])
-.then(([
-  {default: {name: curname}},
-  {default: {name: parname}},
-]) => (
-  process.title += I + parname + I + curname,
-  Promise
-  .all([
-    import(curpath + mdlname + '/index.js'),
-    import(parpath + curname + '.config.js'),
-  ])
-))
-.then(([
-  {default: mdl},
-  {default: cfg},
-]) => 
-  process.argv.slice(2).reduce((acc, cur) => acc && acc[cur], mdl)(cfg)
-)
+	.then(
+		([
+			{
+				default: { name: curname },
+			},
+			{
+				default: { name: parname },
+			},
+		]) => (
+			(process.title = "node" + I + parname + I + curname),
+			Promise.all([
+				import(curpath + theslug + "/index.js").catch(N),
+				import(parpath + curname + ".config.js").catch(N),
+			])
+		)
+	)
+	.then(([{ default: mdl }, { default: cfg }]) =>
+		process.argv.slice(2).reduce((acc, cur) => acc && acc[cur], mdl)(cfg)
+	);
--- dst/index.js
+++ dst/index.js
@@ -1,21 +1,15 @@
-import {mkdir, rmdir, readFile, writeFile} from 'node:fs/promises'
+import { mkdir, rmdir, readFile, writeFile } from "node:fs/promises";
 
-const N = () => undefined
+const N = () => undefined;
 
-const MKDIR = $ => mkdir($, {recursive: true}).catch(N)
-const RMDIR = $ => rmdir($, {recursive: true}).catch(N)
+const MKDIR = ($) => mkdir($, { recursive: true }).catch(N);
+const RMDIR = ($) => rmdir($, { recursive: true }).catch(N);
 
+const build = ({}) =>
+	MKDIR("bin")
+		.then(() => readFile("./node_modules/mdlcfg/bin/index.js"))
+		.then((data) => writeFile("./bin/index.js", data));
 
-const build = ({}) => 
-    MKDIR('bin')
-    .then(() =>
-        readFile('./node_modules/mdlcfg/bin/index.js')
-    )
-    .then(data =>
-        writeFile('./bin/index.js', data)
-    )
+const prune = ({}) => RMDIR("bin");
 
-const prune = ({}) => 
-    RMDIR('bin')
-
-export default {build, prune}
+export default { build, prune };
