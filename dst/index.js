e7dd9de68a30d601f42a548593f9a272e57b4209
stanislavzadiraev <zadiraeff@yandex.ru>
2023-10-07 22:24:04 +0300

    исправлено

--- dst/index.js
+++ dst/index.js
@@ -5,11 +5,11 @@ const N = () => undefined;
 const MKDIR = ($) => mkdir($, { recursive: true }).catch(N);
 const RMDIR = ($) => rmdir($, { recursive: true }).catch(N);
 
-const build = ({}) =>
+const build = () =>
 	MKDIR("bin")
 		.then(() => readFile("./node_modules/mdlcfg/bin/index.js"))
 		.then((data) => writeFile("./bin/index.js", data));
 
-const prune = ({}) => RMDIR("bin");
+const prune = () => RMDIR("bin");
 
 export default { build, prune };
