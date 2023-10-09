804880ec74dbbe3a2375d376103bf29ac6cfa753
stanislavzadiraev <zadiraeff@yandex.ru>
2023-10-09 21:41:05 +0300

    изменено

--- dst/index.js
+++ dst/index.js
@@ -1,24 +1,33 @@
-import { mkdir, rmdir, readFile, writeFile } from "node:fs/promises";
+import { mkdir, rmdir, readFile, writeFile, copyFille } from "node:fs/promises";
 
 const U = () => undefined;
 
 const MKDIR = ($) => mkdir($, { recursive: true }).catch(U);
 const RMDIR = ($) => rmdir($, { recursive: true }).catch(U);
 
+const editjson = (name, data) =>
+	readFile(name)
+		.then((data) => JSON.parse(data))
+		.then((json) =>
+			JSON.stringify(
+				(Object.entries(data).map(
+					(ent) =>
+						(ent[1] == undefined && (delete json[ent[0]], true)) ||
+						((json[ent[0]] = ent[1]), true)
+				),
+				json),
+				null,
+				2
+			)
+		);
+
 const build = () =>
 	MKDIR("bin")
 		.then(() => readFile("./node_modules/mdlcfg/bin/index.js"))
 		.then((data) => writeFile("./bin/index.js", data))
-		.then(() => readFile("./package.json"))
-		.then((data) => JSON.parse(data))
-		.then((json) => JSON.stringify(((json["bin"] = "bin/index.js"), json), null, 2))
-		.then((data) => writeFile("./package.json", data));
+		.then(() => editjson("./package.json", { bin: "bin/index.js" }));
 
 const prune = () =>
-	RMDIR("bin")
-		.then(() => readFile("./package.json"))
-		.then((data) => JSON.parse(data))
-		.then((json) => JSON.stringify((delete json["bin"], json), null, 2))
-		.then((data) => writeFile("./package.json", data));
+	RMDIR("bin").then(() => editjson("./package.json", { bin: unefined }));
 
 export default { build, prune };
