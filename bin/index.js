fd729d6972c6dede0c1f59f9801a8e9b756486ea
stanislavzadiraev <zadiraeff@yandex.ru>
2023-10-13 21:01:05 +0300

    отредактировано

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,24 @@
-#!/usr/bin/env node
+#!/usr/bin / env node
 
 import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
 
 readFile(new URL(join('..', 'package.json'), import.meta.url))
-  .then(data => JSON.parse(data))
-  .then(({ name, main }) =>
-    !name && Promise.reject(new Error('name lost')) ||
-    !main && Promise.reject(new Error('main lost')) ||
-    Promise.resolve({ name, main })
-  )
-  .then(({ name, main }) =>
-    Promise.all([
-      import(join('..', main)).catch(() => ({ default: undefined })),
-      import(join('..', '..', '..', `${name}.config.js`)).catch(() => ({ default: undefined })),
-    ])
-  )
-  .then(([
-    { default: mdl },
-    { default: cfg },
-  ]) =>
-    process.argv.slice(2).reduce((acc, cur) => acc[cur], mdl)(cfg)
-  )
+	.then(data => JSON.parse(data))
+	.then(({ name, main }) =>
+		!name && Promise.reject(new Error('name lost')) ||
+		!main && Promise.reject(new Error('main lost')) ||
+		Promise.resolve({ name, main })
+	)
+	.then(({ name, main }) =>
+		Promise.all([
+			import(join('..', main)).catch(() => ({ default: undefined })),
+			import(join('..', '..', '..', `${name}.config.js`)).catch(() => ({ default: undefined })),
+		])
+	)
+	.then(([
+		{ default: mdl },
+		{ default: cfg },
+	]) =>
+		process.argv.slice(2).reduce((acc, cur) => acc[cur], mdl)(cfg)
+	)
--- dst/index.js
+++ dst/index.js
@@ -1,28 +1,41 @@
-import { readFile, writeFile, cp, rm } from "node:fs/promises";
+import { readFile, writeFile, cp, rm } from "node:fs/promises"
+import { join } from "node:path"
 
 const editjson = (name, data) =>
 	readFile(name)
-		.then((data) => JSON.parse(data))
-		.then((json) =>
-			JSON.stringify(
-				(Object.entries(data).map(
-					(ent) =>
+		.then(data => JSON.parse(data))
+		.then(json => JSON.stringify(
+			(
+				Object.entries(data).map(
+					ent =>
 						(ent[1] == undefined && (delete json[ent[0]], true)) ||
 						((json[ent[0]] = ent[1]), true)
 				),
-					json),
-				null,
-				2
-			)
-		)
-		.then((data) => writeFile(name, data));
+				json
+			),
+			null,
+			2
+		))
+		.then(data => writeFile(name, data));
 
 const build = () =>
-	cp("./node_modules/mdlcfg/bin/", "./bin/", { recursive: true })
-		.then(() => editjson("./package.json", { bin: "bin/index.js" }));
+	readFile(new URL(join('..', 'package.json'), import.meta.url))
+		.then(data => JSON.parse(data))
+		.then(({ bin }) =>
+			(!bin && Promise.reject(new Error("bin lost"))) ||
+			Promise.resolve({ bin })
+		)
+		.then(({ bin }) =>
+			Promise.all([
+				cp(new URL(join('..', 'bin'), import.meta.url), 'bin', { recursive: true }),
+				editjson('package.json', { bin }),
+			])
+		);
 
 const prune = () =>
-	rm("./bin/", { recursive: true })
-		.then(() => editjson("./package.json", { bin: undefined }));
+	Promise.all([
+		rm('bin', { recursive: true }),
+		editjson('package.json', { bin: undefined }),
+	]);
 
 export default { build, prune };
